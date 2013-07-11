47d6fa0610bf5b63355a89e1373014f22f0e2bbb
Olena Miklevska <olena.miklevska@gmail.com>
2013-07-11 15:55:29 +0000

    add index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+app.get('/', function(request, response) 
+  var index_buffer = fs.readFile('index.html');
+  var index_content = index_buffer.toString();
+  response.send(index_content);
 });
 
 var port = process.env.PORT || 5000;
