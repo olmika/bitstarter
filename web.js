b8032068e195b06c55d9dc826782bf3978011e53
olmika <olena.miklevska@gmail.com>
2013-08-10 14:02:33 +0000

    static content, socials, bitcoins

--- web.js
+++ web.js
@@ -2,11 +2,14 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
+app.use('/',express.static(__dirname));
+
 app.get('/', function(request, response){ 
   var index_buffer = fs.readFileSync('index.html');
   var index_content = index_buffer.toString();
   response.send(index_content);
 });
+
 
 var port = process.env.PORT ||8080;
 app.listen(port, function() {
